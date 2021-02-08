ef96fcd7fce60d4a591c7d9f339ab24c55a8516f
root <juferafo@hotmail.com>
2021-02-08 19:21:38 +0000

    [redshift.py] methods and classes moved to lib.py and tested
    [lib.py] file created that contains all the helper callses and methods
    to be used in this repo
    
    [cleanup.py] script that removes the redshift and iam role. Its use is
    intended to avoid unwanted charges. Tested OK.

---
+++ lib.py
@@ -0,0 +1,142 @@
+"""
+This script contains helper classes and methods to automatize the creation of
+the needed resources to run this repository
+"""
+
+import boto3
+import configparser
+import json
+
+class aws_config(str):
+    """
+    This class can be used to
+    """
+
+    def __init__(self, config_path):
+        config = configparser.ConfigParser()
+        config.read_file(open(config_path))
+        
+        # AWS_SECURITY
+        self.key = config.get('AWS_SECURITY','KEY')
+        self.secret = config.get('AWS_SECURITY','SECRET')
+
+        # CLUSTER_PROPERTIES
+        self.region_name            = config.get('CLUSTER_PROPERTIES','REGION_NAME')
+        self.dwh_cluster_type       = config.get("CLUSTER_PROPERTIES","DWH_CLUSTER_TYPE")
+        self.dwh_num_nodes          = config.get("CLUSTER_PROPERTIES","DWH_NUM_NODES")
+        self.dwh_node_type          = config.get("CLUSTER_PROPERTIES","DWH_NODE_TYPE")
+        self.dwh_cluster_identifier = config.get("CLUSTER_PROPERTIES","DWH_CLUSTER_IDENTIFIER")
+        self.db_name                = config.get("CLUSTER_PROPERTIES","DB_NAME")
+        self.db_user                = config.get("CLUSTER_PROPERTIES","DB_USER")
+        self.db_password            = config.get("CLUSTER_PROPERTIES","DB_PASSWORD")
+        self.db_port                = config.get("CLUSTER_PROPERTIES","DB_PORT")
+
+        # IAM_ROLE
+        self.iam_arn           = config.get('IAM_ROLE','ARN')
+        self.iam_role_name = config.get('IAM_ROLE','IAM_ROLE_NAME')
+        
+
+class aws(aws_config):
+    """
+    This class can be used to
+    """
+    
+    def __init__(self, aws_config):
+        
+        aws.iam = boto3.client('iam',\
+                               region_name=aws_config.region_name,\
+                               aws_access_key_id=aws_config.key,\
+                               aws_secret_access_key=aws_config.secret\
+                              )
+
+        aws.redshift = boto3.client('redshift',\
+                                    region_name=aws_config.region_name,\
+                                    aws_access_key_id=aws_config.key,\
+                                    aws_secret_access_key=aws_config.secret\
+                                   )
+        
+
+def iamS3(iam, config):
+    """
+    This method creates a iam role that allows Redshift to read data from S3.
+    """
+    
+    iamrole_dwhS3 = iam.create_role(
+        Path='/',
+        RoleName=config.iam_role_name,
+        Description = "Allows Redshift clusters to call AWS services on your behalf.",
+        AssumeRolePolicyDocument=json.dumps(
+            {'Statement': [{'Action': 'sts:AssumeRole',
+                            'Effect': 'Allow',
+                            'Principal': {'Service': 'redshift.amazonaws.com'}}],
+             'Version': '2012-10-17'})
+    )    
+    
+    print("IAM role {} created".format(config.iam_role_name))
+    
+    iam.attach_role_policy(
+        RoleName=config.iam_role_name,
+        PolicyArn=config.iam_arn
+    )['ResponseMetadata']['HTTPStatusCode']
+    
+    print("IAM policy {} attached to the role {}\n".format(config.iam_arn, config.iam_role_name))
+    
+    rolearn_dwhS3 = iam.get_role(RoleName=config.iam_role_name)['Role']['Arn']
+    
+    return rolearn_dwhS3
+
+
+def create_redshift(redshift, config, role_arn):
+    """
+    This method is used to create a Redshift cluster.
+    
+    Args:
+        REGION_NAME (str):
+        KEY (str):
+        SECRET (str):
+    """
+    
+    response = redshift.create_cluster(     
+        ClusterType       =config.dwh_cluster_type,
+        NodeType          =config.dwh_node_type,
+        NumberOfNodes     =int(config.dwh_num_nodes),
+        DBName            =config.db_name,
+        ClusterIdentifier =config.dwh_cluster_identifier,
+        MasterUsername    =config.db_user,
+        MasterUserPassword=config.db_password,
+        IamRoles          =[role_arn]  
+    )
+    
+    print("Redshift cluster created with properties:\n")
+    
+    print("ClusterType: {}".format(config.dwh_cluster_type))
+    print("NodeType: {}".format(config.dwh_node_type))
+    print("NumberOfNodes: {}".format(config.dwh_num_nodes))
+    print("DBName : {}".format(config.db_name))
+    print("ClusterIdentifier: {}".format(config.dwh_cluster_identifier))
+    #print("MasterUsername : {}".format(config.db_user)
+    #print("MasterUserPassword : {}".format(config.db_password)
+    print("IamRoles: {}\n".format(role_arn))
+
+   
+def cleanup_iam(iam, config):
+    """
+    This method deletes the iam role
+    
+    Args:
+    
+    """
+    
+    iam.detach_role_policy(RoleName=config.iam_role_name, PolicyArn=config.iam_arn)
+    print("IAM policy {} detached from the role {}".format(config.iam_arn, config.iam_role_name))
+    iam.delete_role(RoleName=config.iam_role_name)
+    print("IAM role {} deleted\n".format(config.iam_role_name))
+
+    
+def cleanup_redshift(redshift, config):
+    """
+    This method deletes the redshift cluster
+    """
+    
+    redshift.delete_cluster(ClusterIdentifier=config.dwh_cluster_identifier, SkipFinalClusterSnapshot=True)
+    print("Redshift cluster {} deleted".format(config.dwh_cluster_identifier))
