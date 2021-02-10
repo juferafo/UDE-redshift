3c3c8f45e982be6f871b0b6ba9c2ecd744895a82
root <juferafo@hotmail.com>
2021-02-10 19:09:04 +0000

    [create_tables.py] connection string completed. connection to redshift tested OK

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,8 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
 import boto3
+from lib import aws_config, aws
+from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
     """
@@ -28,19 +29,39 @@ def create_tables(cur, conn):
 
 def main():
     
-    # configparser is used to read the parameters in ./dwh.cfg
-    # Remember to edit ./dwh.cfg according to your needs!
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-    print(*config['CLUSTER'].values())
+    # Remember to edit ../dwh.cfg according to your needs!
+    config_path = '../dwh.cfg'
+    #config = configparser.ConfigParser()
+    #config.read(config_path)
     
-    #conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    #cur = conn.cursor()
+    #reading config file in lib.aws_config
+    config = aws_config(config_path)
 
+    # aws object with iam and redshift clients generated from the config paramenters
+    aws_clients = aws(config)  
+    redshift = aws_clients.redshift
+    
+    cluster_identifier = config.dwh_cluster_identifier
+    redshift_properties = redshift.describe_clusters(ClusterIdentifier=cluster_identifier)['Clusters'][0]
+    
+    host = redshift_properties['Endpoint']['Address']
+    connection_string = "host={} dbname={} user={} password={} port={}".format(host,\
+                                                                               config.db_name,\
+                                                                               config.db_user,\
+                                                                               config.db_password,\
+                                                                               config.db_port)
+    # Careful! This will print a password on the screen!
+    #print(connection_string)
+    
+    conn = psycopg2.connect(connection_string)
+    cur = conn.cursor()
+    print("Connection established to the cluster: redshift://{}:{}/{}".format(host,\
+                                                                              config.db_port,\
+                                                                              config.db_name))
     #drop_tables(cur, conn)
     #create_tables(cur, conn)
 
-    #conn.close()
+    conn.close()
 
 
 if __name__ == "__main__":
